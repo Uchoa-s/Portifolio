3059d081fa608de214932d2bbb6da5db2a451506
Uchoa-s <aucs13579@gmail.com>
2026-04-17 20:21:10 -0300

    Adicionando arquivos iniciais

---
+++ Portifólio/script.js
@@ -0,0 +1,22 @@
+const projetos = [
+  {
+    nome: "Meu Portifólio",
+    descricao: "Painel de Projetos Profissionais",
+    link: "#"
+  },
+];
+
+const container = document.getElementById("lista-projetos");
+
+projetos.forEach(projeto => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <h3>${projeto.nome}</h3>
+    <p>${projeto.descricao}</p>
+    <a href="${projeto.link}" target="_blank">Ver projeto</a>
+  `;
+
+  container.appendChild(card);
+});
